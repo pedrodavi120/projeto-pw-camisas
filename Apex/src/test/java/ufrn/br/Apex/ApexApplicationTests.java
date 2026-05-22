092e5a35bdbacb748b36c2195004fd912d83bfb1
Thiago de Lima <thiagochec3@gmail.com>
2026-05-22 16:31:47 -0300

    commit inicial

---
+++ Apex/src/test/java/ufrn/br/Apex/ApexApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.br.Apex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
