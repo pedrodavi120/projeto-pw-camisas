43e1b3a6aec2754842039b1d9f8031956b312959
pedrodavi120 <pedrodavi120@gmail.com>
2026-05-22 18:19:49 -0300

    feat: padroniza layout estático, corrige chamadas de fragmento e adiciona dependências de segurança

---
+++ src/main/java/ufrn/br/provaPW/ProvaPwApplication.java
@@ -0,0 +1,13 @@
+package ufrn.br.provaPW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaPwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaPwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufrn/br/provaPW/ProvaPwApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.br.provaPW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
